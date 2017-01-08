9d5ccc7ec62651cc7ba197172a45a71c53ea44ac
MenkinaNataliya <tasia-@mail.ru>
2017-01-08 16:31:29 +0500

    изменение таймера с 2 секунд до 1 раза в сутки

--- Server/Program.cs
+++ Server/Program.cs
@@ -16,7 +16,7 @@ namespace Server
             // устанавливаем метод обратного вызова
             TimerCallback tm = new TimerCallback(Count);
             // создаем таймер
-            Timer timer = new Timer(tm, num, 0, 2000);
+            Timer timer = new Timer(tm, num, 0, 86400000);//раз в сутки
 
             Console.ReadLine();
         }
